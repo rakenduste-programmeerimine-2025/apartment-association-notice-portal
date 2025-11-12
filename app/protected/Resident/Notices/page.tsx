dc397920d7b89565f592e08867bfe2a8b64703df
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-11-12 11:25:20 +0200

    refactor(layout):refactor protected layouts for Admin and Resident
    - Split the shared protected layout into separate Admin and Resident layouts, each with their own navigation components and MantineProvider setup.
    - Removed the old shared layout and added a basic Resident Notices page. - Updated UserNavbar to support the new Resident layout.

---
+++ app/protected/Resident/Notices/page.tsx
@@ -0,0 +1,7 @@
+export default function NoticesPage() {
+  return (
+    <div>
+      Resident Notices Page.
+    </div>
+  );
+}
---
+++ app/protected/Resident/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { UserNavbar } from '@/components/NavBar/UserNavbar';
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: '',
+  description: '',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <MantineProvider theme={{ fontFamily: 'Inter, sans-serif' }}>
+      <UserNavbar />
+      {children}
+    </MantineProvider>
+  );
+}
